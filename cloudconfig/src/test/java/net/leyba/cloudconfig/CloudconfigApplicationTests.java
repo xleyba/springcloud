2dc1bc31b28c66aa7888f29d04e8f08db5862bec
Javier Leyba <135663+xleyba@users.noreply.github.com>
2020-09-18 22:06:19 +0200

    Added config server

---
+++ cloudconfig/src/test/java/net/leyba/cloudconfig/CloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leyba.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
