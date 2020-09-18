9b5a3ad8f6ffb6e8d5e649ccc58916a7c1d688aa
Javier Leyba <135663+xleyba@users.noreply.github.com>
2020-09-18 20:46:22 +0200

    Secured Eureka server

--- eurekaserver/src/main/java/net/leyba/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/net/leyba/eurekaserver/EurekaserverApplication.java
@@ -3,6 +3,10 @@ package net.leyba.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 
 @SpringBootApplication
@@ -12,7 +16,16 @@ public class EurekaserverApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(EurekaserverApplication.class, args);
-		//new SpringApplicationBuilder(EurekaserverApplication.class).web(true).run(args);
+	}
+
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
 	}
 
 }
